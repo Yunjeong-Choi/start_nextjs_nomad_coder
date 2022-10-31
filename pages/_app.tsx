50b5c2adadd6ac5754c5c4e24fef6b874863e173
Yunjeong Choi <m.yunjeong.choi@gmail.com>
2022-10-31 18:28:51 +0900

    [MASTER] next.js overview

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { AppProps } from "next/app";
+import NavBar from "../components/NavBar";
+import "../styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <NavBar />
+      <Component {...pageProps} />
+      <style jsx global>{`
+        h1 {
+          color: chocolate;
+        }
+      `}</style>
+    </>
+  );
 }
