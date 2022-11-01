9b50ced401724a7d4c24a061a21f609434764b69
Yunjeong Choi <m.yunjeong.choi@gmail.com>
2022-11-01 15:13:44 +0900

    [MASTER] Layout.tsx 파일 생성

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,16 @@
 import { AppProps } from "next/app";
-import NavBar from "../components/NavBar";
+import Layout from "../components/Layout";
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <NavBar />
+    <Layout>
       <Component {...pageProps} />
       <style jsx global>{`
         h1 {
           color: chocolate;
         }
       `}</style>
-    </>
+    </Layout>
   );
 }
